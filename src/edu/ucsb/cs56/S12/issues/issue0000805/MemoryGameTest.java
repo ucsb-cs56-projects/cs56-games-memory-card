c0b71d70207295c12150a2b7757adabd12afdaa2
Ryan Halbrook <ryanhalbrook@umail.ucsb.edu>
2013-05-16 19:05:49 -0700

    Fixed an issue in MemoryGameTester that was the result of changing the MemoryGrid constructor to take a gridsize

--- src/edu/ucsb/cs56/S12/issues/issue0000805/MemoryGameTest.java
+++ src/edu/ucsb/cs56/S12/issues/issue0000805/MemoryGameTest.java
@@ -16,7 +16,7 @@ public class MemoryGameTest
               //counter to check if count at end is (size/2)-1 ((size/2) nums)
                 int count=0;
 		// make new grid
-                MemoryGrid test1 = new MemoryGrid();
+                MemoryGrid test1 = new MemoryGrid(16);
 		// test that not more than 2 squares share the same num
                 MemoryCard testMem = new MemoryCard();
                 for(int i=1; i<(test1.size/2); i++){
@@ -33,8 +33,8 @@ public class MemoryGameTest
         {
             int i=0,j=0;  //int to increment for each similar value
             //two grids to compare
-            MemoryGrid test1 = new MemoryGrid();
-            MemoryGrid test2 = new MemoryGrid();
+            MemoryGrid test1 = new MemoryGrid(16);
+            MemoryGrid test2 = new MemoryGrid(16);
             MemoryCard temp2 = test2.memGrid.get(j);
             for(MemoryCard temp: test1.memGrid){
                 if(temp.getVal()==temp2.getVal())
@@ -58,14 +58,14 @@ public class MemoryGameTest
 	
 	@Test public void testisOneFlipped()
         {
-            MemoryGrid t1 = new MemoryGrid();
+            MemoryGrid t1 = new MemoryGrid(16);
             t1.memGrid.get(4).flip();
             assertTrue(t1.isOneFlipped());
         }
         
         @Test public void testisTwoFlipped()
         {
-            MemoryGrid t1 = new MemoryGrid();
+            MemoryGrid t1 = new MemoryGrid(16);
             t1.memGrid.get(1).flip();
             t1.memGrid.get(6).flip();
             assertTrue(t1.isTwoFlipped());
@@ -73,14 +73,14 @@ public class MemoryGameTest
         
         @Test public void testgetFlipped()
         {
-            MemoryGrid t1 = new MemoryGrid();
+            MemoryGrid t1 = new MemoryGrid(16);
             t1.memGrid.get(13).flip();
             assertEquals(t1.getFlipped(), 13);
         }
         
         @Test public void testgetFlipped2()
         {
-            MemoryGrid t1 = new MemoryGrid();
+            MemoryGrid t1 = new MemoryGrid(16);
             t1.memGrid.get(10).flip();
             t1.memGrid.get(4).flip();
             assertEquals(t1.getFlipped(10), 4);
@@ -88,7 +88,7 @@ public class MemoryGameTest
         
         @Test public void testisOver()
         {
-            MemoryGrid t1 = new MemoryGrid();
+            MemoryGrid t1 = new MemoryGrid(16);
             t1.isOver=true;
             assertEquals(t1.isOver(), true);
         }
